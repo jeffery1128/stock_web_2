3c7c10f9b14eea2743bca9f9e057ad7ffa817e9b
Jeffrey Tse <57884601+jeffery1128@users.noreply.github.com>
2022-03-31 12:18:36 +0800

    new website source

--- scrap.py
+++ scrap.py
@@ -19,42 +19,41 @@ class scrapper:
 
     def get_dji(self):
         try:
-            self.driver.get('https://www.tradingview.com/markets/indices/quotes-major/')
-            dji_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[3]/td[2]')
-            dji_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[3]/td[4]/span')
-            dji_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[3]/td[3]/span')
+            self.driver.get('https://www.tradingview.com/symbols/DJ-DJI/')
+            time.sleep(5)
+            dji_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[1]/span')
+            dji_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[1]')
+            dji_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[2]')
             self.dji = {'index_name': "道瓊斯工業平均指數",'dji' : dji_index.text, 'dji_change' : dji_change.text, 'dji_percent' : dji_change_percent.text}
             return 0
-        except Exception as e:
+        except:
             print("dji is not successfully scrapped")
-            print(e)
             return 1
 
     def get_hsi(self):
         try:
-            self.driver.get('https://www.tradingview.com/markets/indices/quotes-major/')
-            hsi_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[13]/td[2]')
-            hsi_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[13]/td[4]/span')
-            hsi_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[13]/td[3]/span')
+            self.driver.get('https://www.tradingview.com/symbols/HSI-HSI/')
+            time.sleep(5)
+            hsi_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[1]')
+            hsi_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[1]')
+            hsi_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[2]')
             self.hsi = {'index_name' : "恒生指數",'hsi' : hsi_index.text, 'hsi_change' : hsi_change.text, 'hsi_percent' : hsi_change_percent.text}  
             return 0   
-        except Exception as e:
+        except:
             print("hsi is not successfully scrapped")
-            print(e)
             return 1   
 
     def get_gold(self):
             try:
-                self.driver.get('https://www.tradingview.com/markets/futures/quotes-metals/')
-                time.sleep(0.1)
-                gold_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[2]')
-                gold_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[4]/span')
-                gold_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[2]/div/div/div[2]/div[2]/div/div[2]/div[2]/div/table/tbody/tr[1]/td[3]/span')
+                self.driver.get('https://www.tradingview.com/symbols/COMEX-GC1!/')
+                time.sleep(5)
+                gold_index = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[1]')
+                gold_change = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[1]')
+                gold_change_percent = self.driver.find_element_by_xpath('/html/body/div[2]/div[4]/div[3]/header/div/div[3]/div[1]/div/div/div/div[1]/div[3]/span[2]')
                 self.gold = {"index_name":"倫敦金",'gold' : gold_index.text, 'gold_change' : gold_change.text, 'gold_percent' : gold_change_percent.text}
                 return 0
-            except Exception as e:
+            except:
                 print("gold is not successfully scrapped") 
-                print(e)
                 return 1
 
     def close(self):
